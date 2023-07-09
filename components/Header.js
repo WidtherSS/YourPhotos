b04d609558ebfd3d8721d8c8e9c4e1e03734a357
Adam <adamchatila2020@gmail.com>
2023-07-10 01:29:45 +0200

    fixing warnings

--- components/Header.js
+++ components/Header.js
@@ -7,13 +7,15 @@ const Header = () => {
       <header className="w-full h-28 bg-[#1BBFC5]">
         <nav className="flex justify-between pt-7 px-6 mx-auto max-w-7xl">
           <div className="flex items-center">
-            <Image
-              src="/yourphotos-logo-white.png"
-              alt="YourPhotos Logo"
-              width={180}
-              height={40}
-              layout="fixed"
-            />
+            <Link href="/">
+              <Image
+                src="/yourphotos-logo-white.png"
+                alt="YourPhotos Logo"
+                width={180}
+                height={40}
+                priority={true} // Add the "priority" prop if the image is above the fold
+              />
+            </Link>
           </div>
           <div className="w-8 sm:w-12">
             <Image
@@ -21,6 +23,8 @@ const Header = () => {
               src="/placeholder-img.jpg"
               width={40}
               height={40}
+              alt="profile picture"
+              style={{ objectFit: 'cover', width: '100%', height: '100%' }} // Add the aspect ratio styles if necessary
             />
           </div>
         </nav>
--- components/Section.js
+++ components/Section.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 
 const Section = () => {
   return (
@@ -7,8 +8,17 @@ const Section = () => {
         <h1 className="text-[#1BBFC5] font-bold text-5xl">
           Welcome to YourPhotos!
         </h1>
-        <Link className="font-medium hover:text-2xl" href="/users/1">
-          View User 1's Portfolio
+        <Link className="flex justify-center items-center gap-4 pt-12  font-medium hover:text-xl" href="/users/1">
+          <div className="w-8 sm:w-12">
+            <Image
+              className="rounded-full"
+              src="/placeholder-img.jpg"
+              width={50}
+              height={50}
+              alt="profile picture"
+            />
+          </div>
+          <h3 className="font-bold text-lg">View User 1's Portfolio</h3>
         </Link>
       </section>
     </div>
