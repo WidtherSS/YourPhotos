8b99f1ca57ef20dad2726ae7396e65b3c0af0761
Adam <adamchatila2020@gmail.com>
2023-07-09 21:49:37 +0200

    motions,

--- pages/users/[userid].js
+++ pages/users/[userid].js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import Image from "next/image";
+import { motion } from "framer-motion";
+import Typical from "react-typical";
 import fetch from "isomorphic-unfetch";
 import Link from "next/link";
 import Header from "../../components/Header";
@@ -15,58 +17,112 @@ const UserPage = ({ userData, photosData }) => {
     setIsFollowing((prevIsFollowing) => !prevIsFollowing);
   };
 
+  const userVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, transition: { duration: 0.5 } },
+  };
+
+  const buttonVariants = {
+    rest: { scale: 1 },
+    hover: { scale: 1.1 },
+  };
+
+  const photoVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: { duration: 0.5, staggerChildren: 0.2 },
+    },
+  };
+
+  const thumbnailVariants = {
+    hidden: { opacity: 0, scale: 0.5 },
+    visible: { opacity: 1, scale: 1 },
+  };
+
   return (
     <div>
       <Header />
       <div className="container mx-auto px-4 py-8">
         <div className="flex flex-col items-center pt-8">
           {userData && (
-            <div className="text-center pb-4 sm:pb-7">
+            <motion.div
+              className="text-center pb-4 sm:pb-7"
+              initial="hidden"
+              animate="visible"
+              variants={userVariants}
+            >
               <h1 className="text-3xl sm:text-5xl font-semibold">
-                {userData.name}
+                <Typical
+                  steps={[userData.name, 1000, "Welcome!", 2000]}
+                  loop={Infinity}
+                  wrapper="span"
+                />
               </h1>
               <p className="text-xs sm:text-sm text-[#1BBFC5]">
                 Website: <Link href={userData.website}>{userData.website}</Link>
               </p>
-            </div>
+            </motion.div>
           )}
 
-          <div className="w-32 h-32 sm:w-48 sm:h-48 relative rounded-full overflow-hidden">
+          <motion.div
+            className="w-32 h-32 sm:w-48 sm:h-48 relative rounded-full overflow-hidden"
+            initial="hidden"
+            animate="visible"
+            variants={userVariants}
+          >
             <Image
               src="/placeholder-img.jpg"
               alt={`Avatar of ${userData.name}`}
               layout="fill"
               objectFit="cover"
             />
-          </div>
+          </motion.div>
 
-          <div className="pt-5">
-            <button
+          <motion.div
+            className="pt-5"
+            initial="hidden"
+            animate="visible"
+            variants={userVariants}
+          >
+            <motion.button
               className="w-full sm:w-auto py-3 px-6 rounded-full text-black bg-slate-100 hover:bg-[#1BBFC5] hover:text-white"
               onClick={handleFollowClick}
+              variants={buttonVariants}
+              whileHover="hover"
+              whileTap="hover"
             >
               {isFollowing ? "Unfollow" : "Follow"}
-            </button>
-          </div>
+            </motion.button>
+          </motion.div>
 
           {photosData && (
-            <div className="mt-10">
+            <motion.div
+              className="mt-10"
+              initial="hidden"
+              animate="visible"
+              variants={photoVariants}
+            >
               <h2 className="text-lg sm:text-2xl font-semibold mb-3 sm:mb-5">
                 Photos
               </h2>
               <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
                 {photosData.map((photo) => (
-                  <div key={photo.id} className="rounded-md overflow-hidden">
+                  <motion.div
+                    key={photo.id}
+                    className="rounded-md overflow-hidden"
+                    variants={thumbnailVariants}
+                  >
                     <Image
                       src={photo.thumbnailUrl}
                       alt={`Photo ${photo.id}`}
                       width={200}
                       height={200}
                     />
-                  </div>
+                  </motion.div>
                 ))}
               </div>
-            </div>
+            </motion.div>
           )}
         </div>
       </div>
